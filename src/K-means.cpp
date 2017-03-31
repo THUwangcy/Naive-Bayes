c2e84ddc1b96e6080636db5c93d30a9e6c9ddad8
wang-cy <THUwangcy@gmail.com>
2017-03-31 20:43:21 +0800

    apply K-means dealing missing value

--- src/K-means.cpp
+++ src/K-means.cpp
@@ -16,23 +16,27 @@ using namespace std;
 #define HIGH50 1
 #define alpha 0.2  //Laplace平滑的系数
 #define pi 3.1415926
+#define K 4
 
 double TRAIN_RATE = 1; //用于训练的样本比例
 
 double N[2];  //两种类型人群的总人数，0表示≤50K，1表示>50K
 double totalNum, totalTrain, totalTest;  //数据总数、训练集总数、测试集总数
 vector< vector<string> > samples;  //训练集
+vector< vector<string> > completeSamples;
 map<string, int> features[2][FEATURE_NUM];  //表示某一类别下、某一个特征中该取值在当前类别中的数量，用于取离散值的特征
 double u[2][FEATURE_NUM];  //表示某一类别下，某一特征取值的均值
 double sita[2][FEATURE_NUM];  //表示某一类别下，某一特征取值的方差，这两个都用于连续取值的特征
 bool continuous[FEATURE_NUM] = {1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0};
-string mostPossible[2][FEATURE_NUM];  //表示某一类别下，某一离散取值的特征最可能的取值，用于处理缺失值，这里取训练集出现最多的label作最可能
-double maxLabel[2][FEATURE_NUM];  //表示某一类别下，某一离散取值的特征出现次数最多的那个label的出现次数
 double M[FEATURE_NUM];  //表示某一维特征的最大取值个数（对离散取值而言），用于Laplace平滑
 long success = 0;
 long correct[2] = {0}, pred[2] = {0}, all[2] = {0};
 
 bool check[40000];
+
+vector<int> belongTo;
+vector< vector<string> > clusters(K, vector<string>(FEATURE_NUM, ""));
+double maxValue[FEATURE_NUM];
 
 
 void init() {
@@ -41,23 +45,124 @@ void init() {
 	memset(u, 0, sizeof(u));
 	memset(sita, 0, sizeof(sita));
 	memset(M, 0, sizeof(M));
-	memset(maxLabel, 0, sizeof(maxLabel));
 	memset(check, 0, sizeof(check));
 	srand((unsigned)time(NULL)); 
+	memset(maxValue, 0, sizeof(maxValue));
 }
 
 //计算P(xi|y)，表示在@y类别的条件下，第@i个特征维度取值为@x的概率
 double P(string x, int i, int y) {
 	double result = 0.0;
 	if(continuous[i]) {
-		double num = (x == "?" ? u[y][i] : atof(x.c_str()));
+		double num = atof(x.c_str());
+		num = num / maxValue[i];
 		result = (1 / sqrt(2.0 * pi * sita[y][i])) * exp(-((num - u[y][i]) * (num - u[y][i]) / (2.0 * sita[y][i])));
 	}
 	else {
-		if(x == "?")  x = mostPossible[y][i];
 		result = ((double)features[y][i][x] + alpha) / ((double)N[y] + alpha * M[i]);
 	}
 	return result;
+}
+
+double diff(vector<string> &person1, vector<string> &person2) {
+	double contDiff = 0, disperseDiff = 0;
+	double disperse = 0;
+	for(int i = 0; i < FEATURE_NUM; i ++) {
+		if(person1[i] == "?" || person2[i] == "?")  continue;
+		if(continuous[i]) {
+			double num1 = atof(person1[i].c_str()) / maxValue[i];
+			double num2 = atof(person2[i].c_str()) / maxValue[i];
+			contDiff += (num1 - num2) * (num1 - num2);
+		}
+		else {
+			disperse ++;
+			if(person1[i] != person2[i])  disperseDiff ++;
+		}
+	}
+	contDiff = sqrt(contDiff);
+	disperseDiff /= disperse;
+	return contDiff + disperseDiff;
+}
+
+int classify_cluster(vector<string> &person) {
+	int result;
+	double minDiff = INT_MAX;
+	for(int i = 0; i < K; i ++) {
+		double diff_i = diff(clusters[i], person);
+		if(diff_i < minDiff) {
+			minDiff = diff_i;
+			result = i;
+		}
+	}
+	return result;
+}
+
+void K_means_init() {
+	cout << endl << endl;
+	cout << "********K-means Working**********" << endl;
+	int completeSize = completeSamples.size();
+	for(int i = 0; i < K; i ++) {
+		clusters[i] = completeSamples[rand() % completeSize];
+	}
+	bool flag = true;
+	int iterateCount = 0;
+	while(flag) {
+		iterateCount ++;
+		flag = false;
+		int count[K];  double sum[K][FEATURE_NUM];
+		memset(count, 0, sizeof(count));
+		memset(sum, 0, sizeof(sum));
+		//重新为每个人归类
+		for(int i = 0; i < completeSize; i ++) {
+			int classify_result = classify_cluster(completeSamples[i]);
+			if(classify_result != belongTo[i]) {
+				flag = true;
+				belongTo[i] = classify_result;
+			}
+			count[classify_result] ++;
+		}
+		//记录各个类别中各个特征的信息
+		map<string, int> maxTime[K][FEATURE_NUM];
+		for(int i = 0; i < completeSize; i ++) {
+			for(int j = 0; j < FEATURE_NUM; j ++) {
+				int cluster = belongTo[i];
+				vector<string> person = completeSamples[i];
+				if(continuous[j]) {
+					double num = atof(person[j].c_str());
+					sum[cluster][j] += num;
+				}
+				else {
+					if(maxTime[cluster][j].find(person[j]) != maxTime[cluster][j].end()) {
+						maxTime[cluster][j][person[j]] ++;
+					}
+					else {
+						maxTime[cluster][j][person[j]] = 0;
+					}
+				}
+			}
+		}
+		//重新计算所有簇的中心
+		for(int i = 0; i < K; i ++) {
+			for(int j = 0; j < FEATURE_NUM; j ++) {
+				if(continuous[j]) {
+					clusters[i][j] = to_string(sum[i][j] / count[i]);
+				}
+				else {
+					map<string, int>::iterator it;
+					int Max = 0;
+					for(it = maxTime[i][j].begin(); it != maxTime[i][j].end(); it ++) {
+						if(it -> second > Max) {
+							Max = it -> second;
+							clusters[i][j] = it -> first;
+						}
+					}
+				}
+			}
+		}
+		memset(maxTime, 0, sizeof(maxTime));
+	}
+	cout << "Iterate Times: " << iterateCount << endl;
+	cout << "********K-means Done**********" << endl << endl << endl; 
 }
 
 void outputLoadData() {
@@ -80,29 +185,6 @@ void outputLoadData() {
 		}
 		cout << endl;
 	}
-	cout << endl << "mostPossible: " << endl;
-	string c[2] = {"≤50K", ">50K"};
-	for(int i = 0; i < 2; i ++) {
-		cout << c[i] << ": " << setw(36) << c[0] << setw(20) << c[1] << endl;
-		for(int j = 0; j < FEATURE_NUM; j ++) {
-			string s;
-			if(continuous[j]) {
-				if(u[0][j] != u[1][j])
-					s = to_string(u[i][j]);
-				else 
-					continue;
-			}
-			else {
-				if(mostPossible[0][j] != mostPossible[1][j])
-					s = mostPossible[i][j];
-				else 
-					continue;
-			}
-			cout << setw(20) << s;
-			cout << setw(20) << P(s, j, 0) << setw(20) << P(s, j, 1) << endl;
-		}
-		cout << endl;
-	}
 	cout << endl << "M: " << endl;
 	for(int i = 0; i < FEATURE_NUM; i ++) {
 		if(continuous[i])  cout << "--" << " ";
@@ -121,6 +203,15 @@ void loadData() {
 		vector<string> person = split(s, ',');
 		samples.push_back(person);
 		totalNum ++;
+		int i;
+		for(i = 0; i < FEATURE_NUM; i ++) {
+			if(person[i] == "?") 
+				break;
+		}
+		if(i == FEATURE_NUM) {
+			completeSamples.push_back(person);
+			belongTo.push_back(0);
+		}
 	}
 	for(int k = 0; k < TRAIN_RATE * totalNum; k ++) {
 		int index = ((int)rand() % (int)totalNum);
@@ -136,18 +227,14 @@ void loadData() {
 			if(person[i] == "?")  continue;
 			//连续取值特征第一次遍历只算均值
 			if(continuous[i]) {
-				u[curClass][i] += atof(person[i].c_str());
+				double num = atof(person[i].c_str());
+				if(num > maxValue[i])  maxValue[i] = num;
 			}
 			else {
 				map<string, int>::iterator it = features[curClass][i].find(person[i]);
 				//如果已经记录过该特征
 				if(it != features[curClass][i].end()) {
 					features[curClass][i][person[i]] ++;
-					//更新最可能的该特征值
-					if(features[curClass][i][person[i]] > maxLabel[curClass][i]) {
-						maxLabel[curClass][i] = features[curClass][i][person[i]];
-						mostPossible[curClass][i] = person[i];
-					}
 				}
 				else{
 					features[curClass][i][person[i]] = 1;
@@ -158,20 +245,30 @@ void loadData() {
 			}
 		}
 	}
+	//计算总数
+	totalTrain = N[LOW50] + N[HIGH50];
 	//计算均值
+	for(int i = 0; i < totalTrain; i ++) {
+		int curClass = (samples[i][FEATURE_NUM] == "<=50K." ? LOW50 : HIGH50);
+		for(int j = 0; j < FEATURE_NUM; j ++) {
+			if(!continuous[j])  continue;
+			double num = atof(samples[i][j].c_str());
+			num = num / maxValue[j];
+			u[curClass][j] += num;
+		}
+	}
 	for(int i = 0; i < FEATURE_NUM; i ++) {
 		if(!continuous[i])  continue;
 		u[LOW50][i] /= N[LOW50];
 		u[HIGH50][i] /= N[HIGH50];
 	}
-	//计算总数
-	totalTrain = N[LOW50] + N[HIGH50];
 	//计算方差
 	for(int i = 0; i < totalTrain; i ++) {
 		int curClass = (samples[i][FEATURE_NUM] == "<=50K." ? LOW50 : HIGH50);
 		for(int j = 0; j < FEATURE_NUM; j ++) {
 			if(!continuous[j])  continue;
 			double num = atof(samples[i][j].c_str());
+			num = num / maxValue[j];
 			if(samples[i][j] != "?")  sita[curClass][j] += (u[curClass][j] - num) * (u[curClass][j] - num);
 		}
 	}
@@ -191,8 +288,12 @@ bool classify(vector<string> &person) {
 		double Py = (double)N[ck] / (double)totalTrain;
 		possibility[ck] = log(Py);		
 		for(int i = 0; i < FEATURE_NUM; i ++) {
-			if(person[i] == "?") continue;
-			possibility[ck] = possibility[ck] + log(P(person[i], i, ck));
+			string s = person[i];
+			if(person[i] == "?") {
+				int cluster = classify_cluster(person);
+				s = clusters[cluster][i];
+			}
+			possibility[ck] = possibility[ck] + log(P(s, i, ck));
 		}
 	}
 	int trueClass = (person[FEATURE_NUM] == "<=50K." ? LOW50 : HIGH50);
@@ -209,6 +310,7 @@ bool classify(vector<string> &person) {
 int main() {
 	init();
 	loadData();
+	K_means_init();
 	ifstream fin("../data/adult.test");
 	string s;
 	cout << endl << endl;
